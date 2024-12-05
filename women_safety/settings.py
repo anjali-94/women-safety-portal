d62ae482179717cd79792f9fa625fda1cf8c39bb
anjali-94 <anjalishr987@gmail.com>
2024-12-05 23:14:45 +0530

    seventeen commit

--- women_safety/settings.py
+++ women_safety/settings.py
@@ -83,6 +83,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'admin',
+        'PASSWORD': 'superuser',
+        'HOST': 'your_database_host',
+        'PORT': 'your_database_port',
     }
 }
 
